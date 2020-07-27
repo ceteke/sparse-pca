ff39d2070b161450211d665677552e69665935bf
Cem Eteke <cem@tarentum.io>
2020-07-27 21:37:48 +0300

    ODE of Oja implemented

--- src/solvers.jl
+++ src/solvers.jl
@@ -29,11 +29,23 @@ function online(model, ϕ, τ::Float64, T::Int64)
     η(x) = x - ϕ(x)/p
 
     for t=1:p*T
-        y_k = spiked(1)
+        y_k = model(1)
         x_g = x + (τ/p) .* x * y_k' * y_k
         prox_x = η(x_g)
         x = sqrt(p) * prox_x / norm(prox_x)
         X[t+1,:] = x
     end
     return X
+end
+
+
+function ode(ts::Array{Float64, 1}, Q₀::Float64, τ::Float64, ω::Float64)
+    α₁ = τ*ω*(1+τ/2)
+    α₂ = τ*(ω-τ/2)
+
+    if α₂ == 0
+        sqrt.(1 ./ (2*α₁*ts .+ Q₀^(-2)))
+    else
+        sqrt.(α₂ ./ (α₁ .+ (α₂/(Q₀^2) - α₁)*exp.(-2*α₂*ts)))
+    end
 end
