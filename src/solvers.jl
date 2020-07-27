dc82c54114c17a13bde63a6ffaf76deaca8d4b09
Cem Eteke <cem@tarentum.io>
2020-07-27 18:50:52 +0300

    Online training

--- src/solvers.jl
+++ src/solvers.jl
@@ -17,3 +17,23 @@ function offline(Y::Array{Float64, 2}, τ::Float64, β::Float64, T::Int64)
     end
     return X
 end
+
+
+function online(model, ϕ, τ::Float64, T::Int64)
+    p = size(model(1))[2]
+
+    X = zeros((p*T+1, p))
+    x = randn((1, p)) .* (1/2) .+ 1/sqrt(2)
+    X[1,:] = x
+
+    η(x) = x - ϕ(x)/p
+
+    for t=1:p*T
+        y_k = spiked(1)
+        x_g = x + (τ/p) .* x * y_k' * y_k
+        prox_x = η(x_g)
+        x = sqrt(p) * prox_x / norm(prox_x)
+        X[t+1,:] = x
+    end
+    return X
+end
